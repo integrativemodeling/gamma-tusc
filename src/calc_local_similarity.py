9fc1daa7ad33323f61732899369fa7fc603cf174
cgreenberg <cgreen@salilab.org>
2015-09-08 20:03:50 -0700

    better local alignment

--- src/calc_local_similarity.py
+++ src/calc_local_similarity.py
@@ -13,12 +13,12 @@ import IMP.multifit2
 from optparse import OptionParser
 
 def parse_args():
-    usage = """usage %prog [options] <f1.pdb> <f2.pdb> <chains_for_universe> <chains_for_analysis> <outf>
+    usage = """usage %prog [options] <f1.pdb> <f2.pdb> <chains_for_analysis> <outf>
     Compare two states of the same protein, f1 and f2.
     For each residue, tries to overlap neighborhoods.
     Reports overlap, DRMS and size of the matching nearby particles.
-    Chains for universe: all atoms for picking neighborhoods
     Chains for analysis: look only at these residues
+    Chains for universe: all atoms for picking neighborhoods (will just use whole structure)
     """
     parser=OptionParser(usage)
     parser.add_option("-d","--dist",dest="dist",default=6.0,type='float',
@@ -33,7 +33,7 @@ def parse_args():
                       help="Stop fraction of residues to analyze")
 
     (options, args) = parser.parse_args()
-    if len(args) != 5:
+    if len(args) != 4:
         parser.error("incorrect number of arguments "+str(len(args))+" " + str(args))
     return [options,args]
 
@@ -48,73 +48,65 @@ def get_ps_for_chains(mh,chs):
 
 def run():
     options,args = parse_args()
-    fn1,fn2,chs_univ,chs_analysis,out_fn = args
+    fn1,fn2,chs_analysis,out_fn = args
     mdl = IMP.Model()
-    mh1u = IMP.atom.read_pdb(fn1,mdl)
-    mh2u = IMP.atom.read_pdb(fn2,mdl)
-    mh1a = IMP.atom.read_pdb(fn1,mdl,IMP.atom.CAlphaPDBSelector())
-    mh2a = IMP.atom.read_pdb(fn2,mdl,IMP.atom.CAlphaPDBSelector())
-
+    mh1 = IMP.atom.read_pdb(fn1,mdl)
+    mh2 = IMP.atom.read_pdb(fn2,mdl)
+    rb1 = IMP.atom.create_rigid_body(mh1)
 
     # PS for analysis
-    ps1a = IMP.atom.Selection(mh1a,chain_ids=list(chs_analysis)).get_selected_particles()
-    ps2a = IMP.atom.Selection(mh2a,chain_ids=list(chs_analysis)).get_selected_particles()
+    ps1a = IMP.atom.Selection(mh1,chain_ids=list(chs_analysis),
+                              atom_type=IMP.atom.AtomType("CA")).get_selected_particles()
+    ps2a = IMP.atom.Selection(mh2,chain_ids=list(chs_analysis),
+                              atom_type=IMP.atom.AtomType("CA")).get_selected_particles()
 
     # PS for universe
-    ps1u = IMP.atom.Selection(mh1u,chain_ids=list(chs_univ)).get_selected_particles()
-    ps2u = IMP.atom.Selection(mh2u,chain_ids=list(chs_univ)).get_selected_particles()
+    ps1u = IMP.atom.Selection(mh1).get_selected_particles()
+    ps2u = IMP.atom.Selection(mh2).get_selected_particles()
     vs1u = [IMP.core.XYZ(p).get_coordinates() for p in ps1u]
     vs2u = [IMP.core.XYZ(p).get_coordinates() for p in ps2u]
-    nn1 = IMP.algebra.NearestNeighbor3D(vs1u)
+    nn1 = IMP.algebra.NearestNeighbor3D(vs1u) #use this to find particles that need to match
     nn2 = IMP.algebra.NearestNeighbor3D(vs2u)
 
     # config
-    param = IMP.multifit2.MultiFitParams()
-    query_match_radius = 6.0 #options.dist #5.0
-    dscores = []
     outf = open(out_fn,'w')
     outf.write('# <chain> <residue> <DRMS> <match_len> <len/DRMS>\n')
     num_analysis = len(ps1a)
     start = int(num_analysis * options.start_frac)
     stop = int(num_analysis * options.stop_frac)
     print 'start',start,'stop',stop
+    native_dist = 2.0
     for np,(p1,p2) in enumerate(zip(ps1a,ps2a)):
         if np>=start and np<stop:
             # get info
             res = IMP.atom.get_residue(IMP.atom.Atom(p1))
             this_res = res.get_index()
             this_chain = IMP.atom.Chain(res.get_parent()).get_id()
+
             v1 = IMP.core.XYZ(p1).get_coordinates()
             v2 = IMP.core.XYZ(p2).get_coordinates()
-            print '\n>>>> analyzing',this_chain,this_res
+            #print '\n>>>> analyzing',this_chain,this_res
 
-            # get balls - NOT including the residue itself
-            s1 = set(IMP.atom.Selection(mh1u,chain_id=this_chain,residue_index=this_res).get_selected_particles())
-            s2 = set(IMP.atom.Selection(mh2u,chain_id=this_chain,residue_index=this_res).get_selected_particles())
+            # get particles for neighbor search
+            ball1 = nn1.get_in_ball(v1,options.dist)
 
-            # try expanding radius til you have enough to match
-            test = 0
-            radius = options.dist
-            while test == 0:
-                ball1 = [ps1u[i] for i in nn1.get_in_ball(v1,radius) if ps1u[i] not in s1]
-                ball2 = [ps2u[j] for j in nn2.get_in_ball(v2,radius) if ps2u[j] not in s2]
-                ap = IMP.multifit2.PointAlignment(ball2,param) # TO these ps
-                results = ap.align(ball1,query_match_radius)
-                test = len(results)
-                radius += 0.5
+            # create alignment along backbone and transform
+            bb1 = IMP.atom.Selection(mh1,chain_id=this_chain,residue_indexes=[this_res-1,this_res,this_res+1]).get_selected_particles()
+            bb2 = IMP.atom.Selection(mh2,chain_id=this_chain,residue_indexes=[this_res-1,this_res,this_res+1]).get_selected_particles()
+            trans = IMP.core.get_transformation_aligning_first_to_second(bb1,bb2)
+            IMP.core.transform(rb1,trans)
 
-
-            trans,pairs = results[0]
-            id2,id1 = zip(*pairs)
-            drms = IMP.atom.get_drms([vs1u[i] for i in id1],
-                                     [vs2u[j] for j in id2])
-            print 'radius',radius,'drms',drms,'match size',len(id1),'size/drms',len(id1)/drms
-            del ap
-            #print 'trans',trans
-            #print 'got',len(matches),'matches'
-            #trans,pairs = IMP.multifit2.get_alignment_first_to_second(ball2,ball1)
-            #print 'res',n1,'num pairs',len(pairs),'trans',trans
-            outf.write('%s %i %.5f %i %.5f\n'%(this_chain,this_res,drms,len(id1),float(len(id1))/drms))
+            # get native overlap
+            ngood = 0
+            for idx in ball1:
+                pos = IMP.core.XYZ(ps1u[idx]).get_coordinates()
+                ball2 = nn2.get_in_ball(pos,native_dist)
+                if len(ball2)>0:
+                    ngood+=1
+            frac_good = float(ngood)/len(ball1)
+            IMP.core.transform(rb1,trans.get_inverse())
+            #print 'frac good',frac_good
+            outf.write('%s %i %.5f\n'%(this_chain,this_res,1.0-frac_good))
     outf.close()
 if __name__=="__main__":
     run()
