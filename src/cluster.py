c91ff89504765a5a0cbbc58cbde8cc305ca53a5e
Charles Greenberg <cgreen@salilab.org>
2015-10-02 13:13:18 -0700

    improved cluster

--- src/cluster.py
+++ src/cluster.py
@@ -31,6 +31,10 @@ num_files = int(sys.argv[2])
 fname = sys.argv[3]
 num_clusters = int(sys.argv[4])
 out_dir = sys.argv[5]
+if len(sys.argv)>6:
+    dmat = sys.argv[6]
+else:
+    dmat = None
 display_plot = True
 
 
@@ -38,35 +42,40 @@ try:
     os.mkdir(out_dir)
 except:
     pass
-
-file_list = [os.path.join(file_prefix,str(n),fname) for n in range(1,num_files+1) if os.path.isfile(os.path.join(file_prefix,str(n),fname))]
-my_file_list = IMP.pmi.tools.chunk_list_into_segments(file_list,number_of_processes)[rank]
-my_coords = []
-print 'rank',rank,'len files',len(my_file_list)
-for n,fn in enumerate(my_file_list):
-    mh = IMP.atom.read_pdb(fn,mdl,IMP.atom.CAlphaPDBSelector())
-    coords = [list(IMP.core.XYZ(p).get_coordinates()) for p in IMP.core.get_leaves(mh)]
-    my_coords.append({'gtusc':coords})
-    IMP.atom.destroy(mh)
-    del mh
-    print 'rank',rank,'%.2f'%(float(n+1)/len(my_file_list)*100),'% done'
-
-print 'rank',rank,'scatter 1'
-gathered = IMP.pmi.tools.scatter_and_gather(zip(my_file_list,my_coords))
-#print 'rank',rank,'scatter 2'
-#gather_names = IMP.pmi.tools.scatter_and_gather(my_file_list)
-print 'rank',rank,'cluster fill'
 clusters = IMP.pmi.analysis.Clustering()
-for name,coords in gathered:
-    clusters.fill(name,coords)
 
-print "Global calculating distance matrix"
-clusters.dist_matrix()
+if dmat is None:
+    file_list = [os.path.join(file_prefix,str(n),fname) for n in range(1,num_files+1) if os.path.isfile(os.path.join(file_prefix,str(n),fname))]
+    my_file_list = IMP.pmi.tools.chunk_list_into_segments(file_list,number_of_processes)[rank]
+    my_coords = []
+    print 'rank',rank,'len files',len(my_file_list)
+    for n,fn in enumerate(my_file_list):
+        mh = IMP.atom.read_pdb(fn,mdl,IMP.atom.CAlphaPDBSelector())
+        coords = [list(IMP.core.XYZ(p).get_coordinates()) for p in IMP.core.get_leaves(mh)]
+        my_coords.append({'gtusc':coords})
+        IMP.atom.destroy(mh)
+        del mh
+        print 'rank',rank,'%.2f'%(float(n+1)/len(my_file_list)*100),'% done'
+
+    print 'rank',rank,'scatter 1'
+    gathered = IMP.pmi.tools.scatter_and_gather(zip(my_file_list,my_coords))
+    #print 'rank',rank,'scatter 2'
+    #gather_names = IMP.pmi.tools.scatter_and_gather(my_file_list)
+    print 'rank',rank,'cluster fill'
+    for name,coords in gathered:
+        clusters.fill(name,coords)
+
+    print "Global calculating distance matrix"
+    clusters.dist_matrix()
 if rank==0:
+    if dmat is None:
+        if display_plot:
+            clusters.plot_matrix(figurename=out_dir+"/clusters.png")
+        clusters.save_distance_matrix_file(out_dir+"/clusters.dat")
+    else:
+        print 'loading distance matrix file'
+        clusters.load_distance_matrix_file(dmat)
     clusters.do_cluster(num_clusters)
-    if display_plot:
-        clusters.plot_matrix(figurename=out_dir+"/clusters.png")
-    clusters.save_distance_matrix_file(out_dir+"/clusters.dat")
     for n, cl in enumerate(clusters.get_cluster_labels()):
         print "cluster %s " % str(n)
         print "cluster label %s " % str(cl)
